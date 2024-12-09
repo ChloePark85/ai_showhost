f80fa836f65f5756243ae39a18167bbebddfb4c5
ChloePark85 <hapark85@gmail.com>
2024-12-09 23:54:06 +0900

    added link

--- components/ConvAI.tsx
+++ components/ConvAI.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Conversation } from "@11labs/client";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 async function requestMicrophonePermission() {
   try {
@@ -25,8 +27,6 @@ async function getSignedUrl(): Promise<string> {
   const data = await response.json();
   return data.signedUrl;
 }
-
-import Image from "next/image";
 
 export function ConvAI() {
   const [conversation, setConversation] = useState<Conversation | null>(null);
@@ -70,61 +70,76 @@ export function ConvAI() {
   }
 
   return (
-    <div className={"flex justify-center items-center gap-x-4"}>
-      <Card className={"rounded-3xl bg-red-50"}>
-        <CardContent>
-          <CardHeader>
-            <CardTitle className={"text-center text-red-800"}>
-              {isConnected
-                ? isSpeaking
-                  ? `할머니가 말씀하고 계세요`
-                  : "할머니가 듣고 계세요"
-                : "연결 대기중"}
-            </CardTitle>
-          </CardHeader>
-          <div className={"flex flex-col gap-y-4 text-center"}>
-            <div className="relative w-48 h-48 mx-auto mb-4">
-              <Image
-                src="/grandma.png"
-                alt="인자한 할머니"
-                fill
-                className="rounded-full object-cover"
-                priority
-              />
-            </div>
-            <div
-              className={cn(
-                "orb my-16 mx-12",
-                isSpeaking ? "animate-orb" : conversation && "animate-orb-slow",
-                isConnected ? "orb-active" : "orb-inactive"
-              )}
-            ></div>
+    <div className="relative w-full">
+      <div className={"flex justify-center items-center gap-x-4"}>
+        <Card className={"rounded-3xl bg-red-50"}>
+          <CardContent>
+            <CardHeader>
+              <CardTitle className={"text-center text-red-800"}>
+                {isConnected
+                  ? isSpeaking
+                    ? `할머니가 말씀하고 계세요`
+                    : "할머니가 듣고 계세요"
+                  : "연결 대기중"}
+              </CardTitle>
+            </CardHeader>
+            <div className={"flex flex-col items-center gap-y-4 text-center"}>
+              <div className="relative w-48 h-48 mx-auto mb-1">
+                <Image
+                  src="/grandma.png"
+                  alt="인자한 할머니"
+                  fill
+                  className="rounded-full object-cover"
+                  priority
+                />
+              </div>
+              <p className="text-red-800 font-medium mb-0">AI 할머니</p>
 
-            <Button
-              variant={"outline"}
-              className={
-                "rounded-full text-red-800 border-red-800 hover:bg-red-100"
-              }
-              size={"lg"}
-              disabled={conversation !== null && isConnected}
-              onClick={startConversation}
-            >
-              대화 시작하기
-            </Button>
-            <Button
-              variant={"outline"}
-              className={
-                "rounded-full text-red-800 border-red-800 hover:bg-red-100"
-              }
-              size={"lg"}
-              disabled={conversation === null && !isConnected}
-              onClick={endConversation}
-            >
-              대화 종료하기
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
+              <div
+                className={cn(
+                  "orb my-8 mx-12",
+                  isSpeaking
+                    ? "animate-orb"
+                    : conversation && "animate-orb-slow",
+                  isConnected ? "orb-active" : "orb-inactive"
+                )}
+              ></div>
+
+              <Link
+                href="https://www.nadio.io/fontmarket/?idx=70"
+                target="_blank"
+                className="text-red-800 hover:text-red-600 mb-4 text-sm underline"
+              >
+                내 아바타로 우리 아이와 대화하고 싶다면? <br />
+                나디오 보이스폰트 만들기
+              </Link>
+
+              <Button
+                variant={"outline"}
+                className={
+                  "rounded-full text-red-800 border-red-800 hover:bg-red-100 font-bold"
+                }
+                size={"xl"}
+                disabled={conversation !== null && isConnected}
+                onClick={startConversation}
+              >
+                대화 시작하기
+              </Button>
+              <Button
+                variant={"outline"}
+                className={
+                  "rounded-full text-red-800 border-red-800 hover:bg-red-100 font-bold"
+                }
+                size={"xl"}
+                disabled={conversation === null && !isConnected}
+                onClick={endConversation}
+              >
+                대화 종료하기
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
