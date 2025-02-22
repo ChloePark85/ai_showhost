b948649e7bac15002ea134a85725644b3d64eb06
ChloePark85 <hapark85@gmail.com>
2025-02-22 15:49:06 +0900

    fix: 배경 이미지가 전체 길이로 스크롤되도록 수정

--- components/background-wave.tsx
+++ components/background-wave.tsx
@@ -3,13 +3,12 @@ import { motion } from "framer-motion";
 
 export const BackgroundWave = () => {
   return (
-    <motion.video
-      src="/wave-loop.mp4"
-      autoPlay
-      muted
-      loop
-      controls={false}
-      className="fixed object-cover bottom-0 z-[-1] hidden md:block pointer-events-none opacity-75 filter hue-rotate-[0deg] sepia"
-    />
+    <div className="absolute top-0 left-0 w-full z-[-1] hidden md:block">
+      <motion.img
+        src="/olive.png"
+        alt="olive background"
+        className="w-full h-auto pointer-events-none opacity-75"
+      />
+    </div>
   );
 };
